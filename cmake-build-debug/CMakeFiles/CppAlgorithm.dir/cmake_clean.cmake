c5ef7f418e6df16cddb48bccabd2f5b4530201cc
guangxush <guangxush@163.com>
2019-10-14 15:52:59 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/CppAlgorithm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CppAlgorithm.dir/main.cpp.o"
+  "CppAlgorithm"
+  "CppAlgorithm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CppAlgorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
